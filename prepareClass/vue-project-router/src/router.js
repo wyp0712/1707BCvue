9dfa45425b59a9b27b614a1f90b3dfafeebcf566
devinwang <13263262903@163.com>
2019-10-13 17:14:59 +0800

    vue-router

--- prepareClass/vue-project-router/src/router.js
+++ prepareClass/vue-project-router/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,16 +10,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/home'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/home',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
+    },
+    {
+      path: '/project',
+      name: 'project',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Project.vue')
+    },
+    {
+      path: '/follower',
+      name: 'follower',
+      component: () => import('./views/Follower.vue')
     }
   ]
 })
---
+++ prepareClass/vue-project-router/vue.config.js
@@ -0,0 +1,8 @@
+
+// 此文件每次修改都要重起服务
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    
+  }
+}
